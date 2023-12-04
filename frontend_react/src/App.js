48c4218cc1b588ca8f90a29d033ee82912082a27
Samarth Kamble <kamble.samarth91@gmail.com>
2023-12-04 22:37:22 +0530

    Created background color and some basic styling

--- frontend_react/src/App.js
+++ frontend_react/src/App.js
@@ -1,10 +1,10 @@
 import React from "react";
 import { About, Footer, Header, Skills, Testimonial, Work } from "./container";
 import { Navbar } from "./components";
-
+import "./App.scss";
 const App = () => {
   return (
-    <div>
+    <div className="app">
       <Navbar />
       <Header />
       <About />
