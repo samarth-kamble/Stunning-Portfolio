adc3f56765e2f82955083bcac3424fd20f4e380e
Samarth Kamble <kamble.samarth91@gmail.com>
2023-12-04 19:47:51 +0530

    Sanity Schema Setup completed

---
+++ backend_sanity/schemas/index.js
@@ -0,0 +1,21 @@
+// schemas/index.js
+
+import testimonials from './testimonials'
+import works from './works'
+import brands from './brands'
+import abouts from './abouts'
+import skills from './skills'
+import workExperience from './workExperience'
+import experiences from './experiences'
+import contact from './contact'
+
+export const schemaTypes = [
+  works,
+  testimonials,
+  brands,
+  abouts,
+  skills,
+  workExperience,
+  experiences,
+  contact,
+]
