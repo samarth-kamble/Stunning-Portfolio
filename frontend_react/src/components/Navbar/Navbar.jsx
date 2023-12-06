70789af9a0a8e28febda8cb0230164505622797d
Samarth Kamble <kamble.samarth91@gmail.com>
2023-12-06 23:26:36 +0530

    Completed the hero section and fix the hero image

--- frontend_react/src/components/Navbar/Navbar.jsx
+++ frontend_react/src/components/Navbar/Navbar.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { HiMenuAlt4, HiX } from "react-icons/hi";
 import { motion } from "framer-motion";
 
-import images from "../../constants";
+import { images } from "../../assets/index";
 import "./Navbar.scss";
 
 const Navbar = () => {
---
+++ frontend_react/src/wrapper/index.js
@@ -0,0 +1,4 @@
+// import AppWrap from "./AppWrap";
+// import MotionWrap from "./MotionWrap";
+
+// export { AppWrap, MotionWrap };
