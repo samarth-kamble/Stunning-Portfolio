4375d4d7d79ee1eae77533778930f67d4486ddd7
Samarth Kamble <kamble.samarth91@gmail.com>
2023-12-09 15:54:54 +0530

    Change the contact detail

--- frontend_react/src/container/Footer/Footer.jsx
+++ frontend_react/src/container/Footer/Footer.jsx
@@ -47,8 +47,8 @@ const Footer = () => {
       <div className="app__footer-cards">
         <div className="app__footer-card ">
           <img src={images.email} alt="email" />
-          <a href="mailto:hello@micael.com" className="p-text">
-            hello@micael.com
+          <a href="mailto:samarthkamble0811@gmail.com" className="p-text">
+            samarthkamble0811@gmail.com
           </a>
         </div>
         <div className="app__footer-card">
